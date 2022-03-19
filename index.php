fd98724fbc0ceca5aea840bc3095d18f55749905
N <koonnut22@gmail.com>
2022-03-19 15:44:20 +0700

    test

---
+++ index.php
@@ -0,0 +1,140 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>portfolio</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <!-- BODY -->
+    <div class="G">
+        <div class="pic">
+            <img src="4.jpg" class="img-fluid" alt="Responsive image">
+            <div class="text">welcome</div>
+        </div>    
+    </div>    
+    <div class="container">
+        
+        <h1 class="text-center" style="margin-top: 30px; margin-bottom: 30px;">PORTFOLIO</h1>
+        <div class="text-center">
+            <img style="width: 30%;" src="0dc.png" class="rounded" alt="...">
+          </div>
+        <p class="lead text-justify">
+            <br>
+            <h1 style="text-align: center;">Nutchapon inpirom</h1>
+            <br>
+            <h1>Education</h1>
+            <ul>
+                <li>Tessaban 1 kittikachron school</li>
+                <li>rajamangala university of technology lanna tak</li>
+            </ul>
+            <br>
+            <h1>Hobbies</h1>
+            <ul>
+                <li>Play Game</li>
+                <li>Read Book</li>
+                <li>Eat</li>
+                <li>Sleep</li>
+            </ul>
+        </p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+          </svg>
+          <a href="https://www.facebook.com/koon.nut.7">Nutchapon</a>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+        </svg>
+        <a href="">0654162850</a>
+        <hr>
+        <h2 style="margin-top: 30px; margin-bottom: 30px; text-transform: uppercase;">certificate</h2>
+        <div class="row">
+            <div class="col-md-4" style="margin-bottom: 20px;">
+                <div class="card">
+                    <img class="card-img-top" src="python.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Python certificate</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" style="margin-bottom: 20px;">
+                <div class="card">
+                    <img class="card-img-top" src="java script.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">javascripts certificate</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" style="margin-bottom: 20px;">
+                <div class="card">
+                    <img style="width: 91%;" class="card-img-top" src="cmd.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">cmd certificate</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" style="margin-bottom: 20px;">
+                <div class="card">
+                    <img style="width: 91%;" class="card-img-top" src="basic sql.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">sql certificate</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" style="margin-bottom: 20px;">
+                <div class="card">
+                    <img  class="card-img-top" src="basic program.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">programing certificate</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--footer-->
+<footer class="bg-light text-center text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase"></h5>
+  
+          <p>
+            
+          </p>
+        </div>
+        <!--Grid column-->
+  
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase"></h5>
+  
+          <p>
+           
+          </p>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2077 Copyright:
+    </div>
+    <!-- Copyright -->
+  </footer>
+<!--scropts-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
